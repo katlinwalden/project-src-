4821ec9a740880f659d2b57be6b7572d6948ee16
katlinwalden <48894840+katlinwalden@users.noreply.github.com>
2019-05-05 03:59:26 -0500

    Update MECE5397Project.m

--- MECE5397Project.m
+++ MECE5397Project.m
@@ -1,7 +1,7 @@
 %discretizing the domain
 m=2*pi; %domain length
 nx=200; %1-total nu1mber of x nodes
-ny=400; %1-total nu1mber of y nodes
+ny=200; %1-total nu1mber of y nodes
 dx=m/nx; %gap between x nodes
 dy=m/ny; %gap between y nodes
 x=0:dx:m; %x nodes
@@ -19,23 +19,25 @@ end
 G=transpose(F); %saved in storage for optimizing loops later
     %running actual iterations
 percenterror=1; %predefine for functionality
-s=ones(nx+1,ny+1); %predefine for functionality
+s=1;
 while percenterror >=.01
-    for j=1:ny+1
+    for j=1:ny+1 
+        if j==1
+            u1(:,j)=(m-x).^2.*x; %boundary condition
+        elseif j==ny+1
+                u1(:,j)=(m-x).^2.*cos(x/2); %boundary condition
+        else
         for i=1:nx+1
             if i==1
                 u1(i,j)=m*y(j); %boundary condition
-            elseif j==1
-                u1(i,j)=(m-x(i))^2*x(i); %boundary condition
-            elseif j==ny+1
-                u1(i,j)=(m-x(i))^2*cos(x(i)/2); %boundary condition
             elseif i==nx+1
-                u1(i,j)=.5*(F(i,j)*dx^2*dy^2-u1(i,j-1)-u1(i,j+1)-2*u1(i-1,j));
+                u1(i,j)=.5*(F(i,j)*dx^2*dy^2-u1(i,j-1)-u1(i,j+1)-2*u1(i-1,j)); %Neumann condition with Taylor series substitution f(x-h)=f(x+h)
             else
                 u1(i,j)=.5*(F(i,j)*dx^2*dy^2-u1(i,j-1)-u1(i,j+1)-u1(i-1,j)-u1(i+1,j)); %defnining u1 matrix
             end
         end
+        percenterror=(u1(i,j)-s)/u1(i,j);    
+        s=u1;
+        end      
     end
-    percenterror=max(max(abs((u1-s)/u1)));
-    s=u1;
 end
